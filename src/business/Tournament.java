1f0e0829088fe8fd1dad956419d46ccccb23ae04
Borgar Bordoy <bobor16@student.sdu.dk>
2018-03-07 12:50:28 +0100

    Login Update

--- src/business/Tournament.java
+++ src/business/Tournament.java
@@ -1,15 +1,9 @@
 package business;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author borga
+ * @author gruppe 3
  */
 public class Tournament {
-    
+
 }
