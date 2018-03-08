0e848e385d46996dd42f983d0619c6862e307535
Unknown <oscar.emil.storm@gmail.com>
2018-03-08 13:01:35 +0100

    Update
    nyt sql database og updateret kode

--- src/logic/LogicDB.java
+++ src/logic/LogicDB.java
@@ -18,27 +18,48 @@ public class LogicDB {
         String url = "jdbc:postgresql://baasu.db.elephantsql.com:5432/fcshwhrh";
         String username = "fcshwhrh";
         String password = "-sV1CsXjIa1RYzk7Z6yUxEgsdohJiLoa";
-        System.out.println("playerName\t\tEmail\tNickName\tTeamName\tCountry\tCoach\tTourName\tyear\tPrize\tParticipatingTeams\tWinner");
+        //System.out.println("playerName\t\tNickName\tEmail\tTeamName\tCountry\tCoach\tTourName\tyear\tPrize\tParticipatingTeams\tWinner");
 
         try {
             Connection db = DriverManager.getConnection(url, username, password);
             Statement st = db.createStatement();
-            ResultSet rs = st.executeQuery("select * from players, team, tournement");
-            while (rs.next()) {
+            
+            ResultSet rs1 = st.executeQuery("select coach, tname FROM team");
+            
+            
+           
+            while (rs1.next()) {
                 
-                System.out.print(rs.getString(1) + " | ");
-                System.out.print(rs.getString(2) + " | ");
-                System.out.print(rs.getString(3) + " | ");
-                System.out.print(rs.getString(4) + " | ");
-                System.out.print(rs.getString(5) + " | ");
-                System.out.print(rs.getString(6) + " | ");
-                System.out.print(rs.getString(7) + " | ");
-                System.out.print(rs.getString(8) + " | ");
-                System.out.print(rs.getString(9) + " | ");
-                System.out.print(rs.getString(10) + " | ");
-                System.out.print(rs.getString(11) + " \n ");
+                System.out.print(rs1.getString(1) + "|");
+                System.out.print(rs1.getString(2) + "\n");
+            
+
             }
-            rs.close();
+            rs1.close();
+            db.close();
+            st.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        try {
+            Connection db = DriverManager.getConnection(url, username, password);
+            Statement st = db.createStatement();
+            ResultSet rs1 = st.executeQuery("select * from team");
+            
+            
+           
+            while (rs1.next()) {
+                
+                System.out.print(rs1.getString(1) + "|");
+                System.out.print(rs1.getString(2) + "|");
+                System.out.print(rs1.getString(3) + "\n");
+                
+            
+
+            }
+            rs1.close();
+            
             st.close();
 
         } catch (Exception e) {
